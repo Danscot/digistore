2305b97c3fb9a5d84aa8c7fea76971f7015f8d6a
Danscot <scotfielddaniel@gmail.com>
2026-01-20 04:28:31 +0100

    Fixed some productions errors

--- digistore/settings.py
+++ digistore/settings.py
@@ -124,11 +124,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# settings.py
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/digistore/staticfiles/'  # This is what Nginx will serve
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/digistore/media/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
