cfebbd1788b6cfc0d357cb5a65d585d8a1ea18ba
Danscot <scotfielddaniel@gmail.com>
2026-02-01 21:47:07 +0100

    fixing stuffs

--- devOps/management/commands/backup_db_sqlite.py
+++ devOps/management/commands/backup_db_sqlite.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from devOps.tasks import backup_db_sqlite
 
 class Command(BaseCommand):
+    
     help = "Backup SQLite DB and send via email"
 
     def handle(self, *args, **kwargs):
