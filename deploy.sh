4f73abd5b16939a2404e4908dd5f4c0a3e7de6f3
Danscot <scotfielddaniel@gmail.com>
2026-02-01 20:47:25 +0100

    adding some proper logs

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,34 @@ set -e
 
 echo "🚀 Starting deployment..."
 
+
+echo "🚀 activating env..."
+
 cd /home/ubuntu/digistore
 
 source venv/bin/activate
 
+
+echo "🚀 pulling up from github..."
+
 git pull --rebase
 
+
+echo "🚀 installing requirements.."
+
 pip install -r requirements.txt
+
+
+echo "🚀 DB updates..."
 
 python manage.py makemigrations
 
 python manage.py migrate
 
 python manage.py collectstatic --noinput
+
+
+echo "🚀 reloading services..."
 
 sudo systemctl reload nginx
 
